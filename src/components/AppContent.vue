e2dcbd5b19aa2dcf3ce2078cb609d25209a75926
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-06-12 19:16:19 +0200

    Milestone 4

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,11 +1,23 @@
 <script>
+import axios from 'axios';
 
 export default {
   components: {
   },
   data() {
     return {
+      projects: []
     }
+  },
+  methods: {
+    fetchData() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+        this.projects = res.data.projects;
+      })
+    }
+  },
+  created() {
+    this.fetchData();
   }
 }
 
@@ -17,8 +29,12 @@ export default {
     <section>
       <div class="container py-5">
         <div class="row aling-items-center flex-column justify-content-center">
-          <div class="col-auto text-center">
-            <h1></h1>
+          <div class="col-auto">
+            <ul class="list-unstyled">
+              <li v-for="project in projects">
+                <h3><span class="text-danger">Project Title:</span> {{ project.name }}</h3>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
